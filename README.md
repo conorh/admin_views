8bbaf5417409c2f69265ff502e341e631b6c3e91
Conor Hunt <conor.hunt+git@gmail.com>
2010-02-23 00:33:33 -0500

    removed dependency on make_resourceful

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Install
 
     gem install admin_views
 
-This library uses the will\_paginate, formtastic and make\_resourceful gems.
+This library uses the will\_paginate and formtastic gems.
 
 Usage
 -----
--- generators/admin_views/templates/controllers/controller.rb
+++ generators/admin_views/templates/controllers/controller.rb
@@ -1,17 +1,56 @@
 class Admin::<%= pluralize(model_class) %>Controller < Admin::AdminController
-  # http://github.com/hcatlin/make_resourceful
-  make_resourceful do
-    actions :all
-  end
-
+  # GET /admin/<%= model_plural %>
   def index
-    # TODO: Add in sorting
-
     if params[:search]
       # TODO: By default search all text fields
       @<%= model_plural %> = <%= model_class %>.paginate(:all, :page => params[:page])
     else
       @<%= model_plural %> = <%= model_class %>.paginate(:all, :page => params[:page])
     end
+  end
+ 
+  # GET /admin/<%= model_plural %>/1
+  def show
+    @<%= model_name %> = <%= model_class %>.find(params[:id])
+  end
+ 
+  # GET /admin/<%= model_plural %>/new
+  def new
+    @<%= model_name %> = <%= model_class %>.new
+  end
+ 
+  # GET /admin/<%= model_plural %>/1/edit
+  def edit
+    @<%= model_name %> = <%= model_class %>.find(params[:id])
+  end
+ 
+  # POST /admin/<%= model_plural %>
+  def create
+    @<%= model_name %> = <%= model_class %>.new(params[:<%= model_name %>])
+ 
+    if @<%= model_name %>.save
+      redirect_to(@<%= model_name %>, :notice => '<%= class_name %> was successfully created.')
+    else
+      render :action => "new"
+    end
+  end
+ 
+  # PUT /admin/<%= model_plural %>/1
+  def update
+    @<%= model_name %> = <%= model_class %>.find(params[:id])
+ 
+    if @<%= model_name %>.update_attributes(params[:<%= model_name %>])
+      redirect_to(admin_<%= model_plural %>_url(@<%= model_name %>), :notice => '<%= model_class %> was successfully updated.')
+    else
+      render :action => "edit"
+    end
+  end
+ 
+  # DELETE /admin/<%= model_plural %>/1
+  def destroy
+    @<%= model_name %> = <%= model_class %>.find(params[:id])
+    @<%= model_name %>.destroy
+
+    redirect_to(admin_<%= model_plural %>_url)
   end
 end
